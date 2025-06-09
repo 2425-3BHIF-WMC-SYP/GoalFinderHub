21ecbca297980b1dc8388b8175db9380feaf9068
alexcoroi <a.coroi@students.htl-leonding.ac.at>
2025-06-09 14:53:28 +0200

    Added websockets

--- src/frontend/src/fetch-rest-endpoint.ts
+++ src/frontend/src/fetch-rest-endpoint.ts
@@ -1,42 +1,47 @@
+export function getServerUrl(): string {
+	let apiUrl: string
+
+	if (import.meta.env.DEV) {
+		apiUrl = 'http://localhost:3000'
+	} else {
+		apiUrl = 'http://goalfinderhub.local:3000'
+	}
+
+	return apiUrl
+}
+
 function getApiUrl(): string {
-  let apiUrl: string
-
-  if (import.meta.env.DEV) {
-    apiUrl = 'http://localhost:3000/api'
-  } else {
-    apiUrl = 'http://goalfinderhub.local:3000/api'
-  }
-
-  return apiUrl
+	const serverUrl = getServerUrl()
+	return `${serverUrl}/api`
 }
 
 export async function fetchRestEndpoint(
-  route: string,
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
-  data?: object,
+	route: string,
+	method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+	data?: object,
 ): Promise<any> {
-  const options: RequestInit = { method }
+	const options: RequestInit = { method }
 
-  const headers = new Headers()
-  const jwt = sessionStorage.getItem('jwt')
-  if (jwt !== null) {
-    headers.append('Authorization', `Bearer ${jwt}`)
-  }
-  if (data) {
-    headers.append('Content-Type', 'application/json')
-    options.body = JSON.stringify(data)
-  }
-  options.headers = headers
+	const headers = new Headers()
+	const jwt = sessionStorage.getItem('jwt')
+	if (jwt !== null) {
+		headers.append('Authorization', `Bearer ${jwt}`)
+	}
+	if (data) {
+		headers.append('Content-Type', 'application/json')
+		options.body = JSON.stringify(data)
+	}
+	options.headers = headers
 
-  const apiUrl = getApiUrl()
+	const apiUrl = getApiUrl()
 
-  const res = await fetch(`${apiUrl}${route}`, options)
+	const res = await fetch(`${apiUrl}${route}`, options)
 
-  if (!res.ok) {
-    const message = await res.text()
-    throw new Error(`${message}`)
-  }
-  if (res.status !== 204) {
-    return await res.json();
-  }
+	if (!res.ok) {
+		const message = await res.text()
+		throw new Error(`${message}`)
+	}
+	if (res.status !== 204) {
+		return await res.json()
+	}
 }
